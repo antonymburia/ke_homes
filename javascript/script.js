42f9683d506bc0aaba34ba8b19fc8685612d4e9b
antonymburia <antonymburia10@gmail.com>
2022-01-19 19:06:26 +0300

    create a filter functionality

--- javascript/script.js
+++ javascript/script.js
@@ -7,4 +7,26 @@ function filterresults(){
   var onebr = 20000;
   var twobr = 30000;
   var threebr = 40000;
+
+  // filter results
+  if (room == 'studio' && location == 'juja' && maxprice >= studioapt) {
+    var result = document.getElementById("studio");
+    
+   } 
+   else {
+     if (room == '1bdr' && location == 'kahawa' && maxprice >= onebr) {
+      var result = document.getElementById("1");
+     } else {
+       if (room == '2bdr' && location == 'kilimani' && maxprice >= twobr) {
+        var result = document.getElementById("2");
+       } else {
+         if (room == '3bdr' && location == 'gong' && maxprice >= threebr) {
+          var result = document.getElementById("3");
+         } else {
+           document.getElementById("results").innerHTML = 'Sorry there are no results matching your search';
+           return false;
+         }
+       }
+     }
+   }
 }
