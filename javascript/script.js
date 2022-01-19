cf7ecf7697a19720d205bce11c152d4e15882151
antonymburia <antonymburia10@gmail.com>
2022-01-19 19:05:08 +0300

    get values from html

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
-t.js
+// t.js
+function filterresults(){
+  var room = document.getElementById('rooms').value;
+  var location = document.getElementById('location').value;
+  var maxprice = document.getElementById('price').value;
+  var studioapt = 15000;
+  var onebr = 20000;
+  var twobr = 30000;
+  var threebr = 40000;
+}
